0b320881e9ca2c7fe7a77cc2f6764796847c6456
cristiano <britonano@gmail.com>
2021-03-19 15:48:30 -0300

    Update conectasql2.js

--- conectasql2.js
+++ conectasql2.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('blog', '', '', {
+const sequelize = new Sequelize('', '', '', {
       host:"localhost",
       dialect:'mysql'
 })
